663e02166198e0a5c27fe524b1316fa5f946a064
“anish786“ <ali.anish91@yahoo.com>
2018-06-09 16:24:02 -0400

    Game Logic

--- matchTheColor/Template/Settings.swift
+++ matchTheColor/Template/Settings.swift
@@ -6,4 +6,10 @@
 //  Copyright © 2018 Anish Ali. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+
+enum PhysicsCategories {
+    static let none: UInt32 = 0
+    static let ballCategory: UInt32 = 0x1 // 01
+    static let switchCategory: UInt32 = 0x1 << 1 // 10
+}
